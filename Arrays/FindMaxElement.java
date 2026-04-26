cbf67305ec285ab7a397affdd06208da056a4875
Ashi Sugandhi <119334950+ashi2116@users.noreply.github.com>
2026-04-26 13:21:59 +0530

    Implement FindMaxElement class to find maximum value

---
+++ Arrays/FindMaxElement.java
@@ -0,0 +1,27 @@
+class FindMaxElement {
+
+    public static int findMax(int[] arr) {
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 7, 2, 9, 5};
+
+        int result = findMax(arr);
+
+        System.out.println("Maximum element is: " + result);
+
+    }
+}
